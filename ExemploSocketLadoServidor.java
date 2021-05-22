ca85bc498a312c5d1a31311e66b740d77de8d6af
Duran <42320816+deraldoduran@users.noreply.github.com>
2021-05-22 20:13:48 -0300

    corrigido e entregue

--- ExemploSocketLadoServidor.java
+++ ExemploSocketLadoServidor.java
@@ -1,24 +1,20 @@
 
 
-package exemplosocketladoservidor;
 import java.net.*;
 import java.io.*;
 
-/**
- *
- * @author duran
- */
-public class ExemploSocketLadoServidor {
-    
-public static void main (String args []){
-try{
-int serverPort = 7896;
-ServerSocket listenSocket = new ServerSocket (serverPort);
-while(true) {
-    Socket clientSocket = listenSocket.accept();
-    Connection c = new Connection(clientSocket);
-}
-} catch(IOException e) { System.out.println ("Listen:" + e.getMessage());
-}
-}
+public class TCPServer {
+    public static void main (String args []){
+        try{
+            int serverPort = 7896;
+            ServerSocket listenSocket = new ServerSocket (serverPort);
+            while(true) {
+                Socket clientSocket = listenSocket.accept();
+                Connection c = new Connection(clientSocket);
+                c.run();
+            }
+        } catch(IOException e) {
+            System.out.println ("Listen:" + e.getMessage()); 
+        }
+    }
 }
