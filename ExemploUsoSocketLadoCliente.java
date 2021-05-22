00f973ae8361fd4c447a5e37959ba4af70b19742
Duran <42320816+deraldoduran@users.noreply.github.com>
2021-05-22 20:29:07 -0300

    corrigido e entregue

--- ExemploUsoSocketLadoCliente.java
+++ ExemploUsoSocketLadoCliente.java
@@ -1,40 +1,34 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package exemplousosocketladocliente;
 import java.net.*;
 import java.io.*;
 
-/**
- *
- * @author duran
- */
-public class ExemploUsoSocketLadoCliente {
-
-
-public static void main (String args []){
-// arguments supply message and hostname of destination
-Socket s = null;
-try{
-int serverPort = 7896;
-s = new Socket(args [1], serverPort );
-DataInputStream in = new DataInputStream ( s.getInputStream());
-DataOutputStream out = new DataOutputStream ( s.getOutputStream());
-out.writeUTF (args [0]); // UTF is a string encoding see Sn 4.3
-String data = in.readUTF();
-System.out.println ("Received: "+ data) ;
-}catch (
-UnknownHostException e){
-System.out.println ("Sock:" + e.getMessage());
-}catch (EOFException e){ System.out.println ("EOF:" + e.getMessage());
-}catch (IOException e){ System.out.println ("IO:" + e.getMessage());
-}finally {if(s!=null) try {
-s.close ();}catch (IOException e)
-{ System.out.println ("Close:" + e.getMessage());
-}
-}
-    
-}
+public class TCPClient {
+    public static void main (String args[]) {
+        // arguments supply message and hostname of destination
+        Socket s = null;
+        try{
+            int serverPort = 7896;
+            s = new Socket("localhost", serverPort);
+            DataInputStream in = new DataInputStream( s.getInputStream());
+            DataOutputStream out = new DataOutputStream( s.getOutputStream());
+            // Envio da mensagem ao Server - Socket (ip, porta)
+            out.writeUTF("[Cliente] 'Olá mundo!'"); // UTF is a string encoding
+            // Retorno da mensagem enviada ao Server
+            String data = in.readUTF();
+            //Imprime no Cliente a mensagem recebida
+            System.out.println("Received: " + data);
+        } catch (UnknownHostException e){
+            System.out.println("Sock:" + e.getMessage());
+        } catch (EOFException e){
+            System.out.println("EOF:" + e.getMessage());
+        } catch (IOException e){
+            System.out.println("IO:" + e.getMessage());
+        } finally {
+            if(s != null)
+            try {
+                s.close();
+            } catch (IOException e){
+                System.out.println("close:" + e.getMessage());
+            }
+        }
+    }
 }
