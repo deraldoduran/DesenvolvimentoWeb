698231b061db1877dd77aca7cb4a7498477018fe
Duran <42320816+deraldoduran@users.noreply.github.com>
2021-05-22 20:12:40 -0300

    corrigido e entregue

--- Connection.java
+++ Connection.java
@@ -1,46 +1,36 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package exemplosocketladoservidor;
+import java.net.*;
+import java.io.*;
 
-import java.io.DataInputStream;
-import java.io.DataOutputStream;
-import java.io.EOFException;
-import java.io.IOException;
-import java.net.Socket;
-
-/**
- *
- * @author duran
- */
 public class Connection extends Thread {
     DataInputStream in;
     DataOutputStream out;
     Socket clientSocket;
-    public Connection(Socket aClientSocket){
-        try{
+    
+    public Connection (Socket aClientSocket) {
+        try {
             clientSocket = aClientSocket;
-            in = new DataInputStream(clientSocket.getInputStream());
-            out = new DataOutputStream(clientSocket.getOutputStream());
+            in = new DataInputStream( clientSocket.getInputStream());
+            out = new DataOutputStream( clientSocket.getOutputStream());
             this.start();
-            
-        }catch(IOException e){System.out.println("Connection:" + e.getMessage());
+        } catch(IOException e) {
+            System.out.println("Connection:" + e.getMessage());
+        }
     }
     
     public void run(){
-        try{
+        try { // an echo server
             String data = in.readUTF();
-            out.writeUTF(data);
-        }catch(EOFException e){System.out.println("EOF:" e.getMessage());
-        
-        }catch(IOException e){System.out.println("IO:" + e.getMessage());}
-    }finally {try {clientSocket.close();}catch(IOException e){/*close failed*/}}
-        
-    
-            
+            out.writeUTF("[Server] > " + data);
+        } catch(EOFException e) {
+            System.out.println("EOF:" + e.getMessage());
+        } catch(IOException e) {
+            System.out.println("IO:" + e.getMessage());
         }
-    
-
+        
+        try {
+            clientSocket.close();
+        } catch (IOException e) {
+            System.out.println("IO:" + e.getMessage());
+        }
+    }
 }
